9ba62d99c2effac734e135ef6a7c5b9b67b13f67
Paige Bolduc <paigebolduc@gmail.com>
2017-03-25 21:16:27 -0400

    Try to fix spacing again.

--- exercises/chapter_01/01.01.scm
+++ exercises/chapter_01/01.01.scm
@@ -3,26 +3,26 @@
 ;; Below is a sequence of expressions. What is the result printed by the interpreter in response to each expression?
 ;; Assume that the sequence is to be evaluated in the order in which it is presented.
 
-(+ 5 3 4) 							 ;12
-(- 9 1) 							 ;8
-(/ 6 2) 							 ;3
-(+ (* 2 4) (- 4 6))  				 ;6
-(define a 3) 						 ;a
-(define b (+ a 1)) 					 ;b
-(+ a b (* a b)) 					 ;19
-(= a b) 							 ;#f
+(+ 5 3 4) 												;12
+(- 9 1) 							 						;8
+(/ 6 2) 							 						;3
+(+ (* 2 4) (- 4 6))  				 			;6
+(define a 3) 						 					;a
+(define b (+ a 1)) 					 			;b
+(+ a b (* a b)) 					 				;19
+(= a b) 							 						;#f
 
 (if (and (> b a) (< b (* a b)))
 	b
-	a) 								 ;4
+	a) 								 							;4
 
 (cond ((= a 4) 6)
-	((= b 4) (+ 6 7 a))
-	(else 25)) 						 ;16
+			((= b 4) (+ 6 7 a))
+			(else 25)) 						 			;16
 
-(+ 2 (if (> b a) b a)) 				 ;6
+(+ 2 (if (> b a) b a)) 				 		;6
 
-(* (cond ((> a b) a)
-		 ((< a b) b)
-		 (else -1))
-	(+ a 1)) 						 ;16
+(*  (cond ((> a b) a)
+						((< a b) b)
+						(else -1))
+	  (+ a 1)) 						 					;16
