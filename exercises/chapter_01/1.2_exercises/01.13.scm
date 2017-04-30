cd1ee7b851ef0196fe75023ffdd8a1facaeccaff
Paige Bolduc <paigebolduc@gmail.com>
2017-04-30 11:08:05 -0400

    Add 01.13 question- unfinished

---
+++ exercises/chapter_01/1.2_exercises/01.13.scm
@@ -0,0 +1,7 @@
+;Prove that Fib(n)Fib(n) is the closest integer to ϕn/5‾√ϕn/5, where
+;ϕ=1+5‾√2≈1.6180339887
+;Hint: Let
+;ψ=1−5‾√2=1−ϕ=−1ϕ≈−0.6180339887
+;
+;Use induction and the definition of the Fibonacci numbers (see section 1.2.2) to prove that
+;Fib(n)=ϕn−ψnϕ−ψ=ϕn−ψn5‾√
