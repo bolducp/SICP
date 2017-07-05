d51ec073badd55902fa694b003ab78e7bdd3310d
Paige Bolduc <paigebolduc@gmail.com>
2017-07-04 17:41:18 -0700

    Add 01.24 solution

--- exercises/chapter_01/1.2_exercises/01.24.scm
+++ exercises/chapter_01/1.2_exercises/01.24.scm
@@ -23,23 +23,13 @@
         ((fermat-test n) (fast-prime? n (- times 1)))
       (else false)))
 
-
 (define (square x) (* x x))
-
-(define (smallest-divisor n)
- (find-divisor n 2))
-
-(define (divides? a b)
- (= (remainder b a) 0))
-
-(define (prime? n)
- (= n (smallest-divisor n)))
 
 (define (timed-prime-test n)
  (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time)
- (if (prime? n)
+ (if (fast-prime? n 100)
      (report-prime n (- (runtime) start-time))))
 
 (define (report-prime n elapsed-time)
@@ -47,3 +37,18 @@
  (display n)
  (display " *** ")
  (display elapsed-time))
+
+(timed-prime-test 1009)
+(timed-prime-test 1013)
+(timed-prime-test 1019)
+(timed-prime-test 100000000003)
+(timed-prime-test 100000000019)
+(timed-prime-test 100000000057)
+(timed-prime-test 100000000000051)
+(timed-prime-test 100000000000099)
+(timed-prime-test 10000000000000000099)
+(timed-prime-test 100000000000000000099)
+
+; Since the Fermat test has Θ(logn) growth, I'd expect to find that primes near 1,000,000 take about
+; twice as long as primes near 1,000. However, the interpreter is so fast, that this difference doesn't
+; actually seem to be reflected in the timed-prime-test results.
