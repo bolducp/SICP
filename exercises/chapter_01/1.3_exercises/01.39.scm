9f336463f2441b89502a2e623c0dfd31d812ed77
Paige Bolduc <paige@smartlogic.io>
2017-11-05 21:14:15 -0500

    Add 1.39 solution

--- exercises/chapter_01/1.3_exercises/01.39.scm
+++ exercises/chapter_01/1.3_exercises/01.39.scm
@@ -4,3 +4,21 @@
 ;Define a procedure (tan-cf x k) that computes an approximation to the tangent
 ;function based on Lambert's formula. k specifies the number of terms to compute,
 ;as in Exercise 1-37.
+
+
+(define (cont-frac-iterative n d k)
+  (define (iterative-frac i acc)
+    (if (= i 0) acc
+               (iterative-frac (- i 1)(/ (n i)
+                                         (+ (d i) acc)))))
+(iterative-frac k (/ (n k)(d k))))
+
+(define (square x) (* x x))
+
+(define (d i) (- (* 2 i) 1))
+
+(define (tan-cf x k)
+  (define (n i)
+    (if (> i 1) (- (square x))
+                x))
+  (cont-frac-iterative n d k))
